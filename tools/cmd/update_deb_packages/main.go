8cecc2ed0ba632b593b18f2b7df0bf386eb3767c
Peter Mylemans <peter.mylemans@gmail.com>
2020-11-13 18:10:28 +0100

    Generate all architectures

--- tools/cmd/update_deb_packages/main.go
+++ tools/cmd/update_deb_packages/main.go
@@ -9,12 +9,27 @@ import (
 )
 
 func main() {
-	content, err := ioutil.ReadFile("../package_bundle_amd64_debian9.bzl")
+	updateStretch("amd64", "9", "stretch")
+	updateStretch("arm64", "9", "stretch")
+	updateStretch("ppc64le", "9", "stretch")
+	updateStretch("s390x", "9", "stretch")
+	updateStretch("amd64", "10", "buster")
+	updateStretch("arm64", "10", "buster")
+	updateStretch("ppc64le", "10", "buster")
+	updateStretch("s390x", "10", "buster")
+}
+
+func updateStretch(arch string, version string, distro string) {
+	debArch := arch
+	if arch == "ppc64le" {
+		debArch = "ppc64el"
+	}
+	content, err := ioutil.ReadFile("../package_bundle_" + arch + "_debian" + version + ".bzl")
 	if err != nil {
 		log.Fatal(err)
 	}
 
-	row, err := packages.ParseBundle("package_bundle_amd64_debian9.bzl", content)
+	row, err := packages.ParseBundle("package_bundle_"+arch+"_debian"+version+".bzl", content)
 
 	packagesFile, err := os.Open("../Packages")
 	if err != nil {
@@ -22,25 +37,29 @@ func main() {
 	}
 	defer packagesFile.Close()
 
-	arch := "amd64"
-	err = row.UpdateFromPackageIndex("debian", "http://deb.debian.org/debian", "stretch", "main", arch)
+	err = row.UpdateFromPackageIndex("debian", "http://deb.debian.org/debian", distro, "main", debArch)
 	if err != nil {
 		log.Fatal(err)
 	}
-	err = row.UpdateFromPackageIndex("debian", "http://deb.debian.org/debian", "stretch-updates", "main", arch)
+	err = row.UpdateFromPackageIndex("debian", "http://deb.debian.org/debian", distro+"-updates", "main", debArch)
 	if err != nil {
 		log.Fatal(err)
 	}
-	err = row.UpdateFromPackageIndex("debian", "http://deb.debian.org/debian", "stretch-backports", "main", arch)
-	if err != nil {
-		log.Fatal(err)
-	}
-	err = row.UpdateFromPackageIndex("debian-security", "http://deb.debian.org/debian-security", "stretch/updates", "main", arch)
-	if err != nil {
-		log.Fatal(err)
+	if distro == "stretch" {
+		err = row.UpdateFromPackageIndex("debian", "http://deb.debian.org/debian", distro+"-backports", "main", debArch)
+		if err != nil {
+			log.Fatal(err)
+		}
 	}
 
-	newFile, err := os.Create("../package_bundle_amd64_debian9.bzl")
+	if arch == "amd64" || arch == "arm64" {
+		err = row.UpdateFromPackageIndex("debian-security", "http://deb.debian.org/debian-security", distro+"/updates", "main", debArch)
+		if err != nil {
+			log.Fatal(err)
+		}
+	}
+
+	newFile, err := os.Create("../package_bundle_" + arch + "_debian" + version + ".bzl")
 	if err != nil {
 		log.Fatal(err)
 	}
@@ -49,5 +68,4 @@ func main() {
 	if err != nil {
 		log.Fatal(err)
 	}
-
 }
